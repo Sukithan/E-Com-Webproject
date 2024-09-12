8b6fdf268845caaa74004754ea2f64e03a49456c
RahaviSiri <rahavi24siri@gmail.com>
2024-09-12 20:23:02 +0530

    Remove some unwanted codes at NAVBAR

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -29,80 +29,70 @@ const Home = () => {
       
       {/* Promotions Section */}
       <section className='promotions my-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-        <div className='bg-blue-200 p-6 rounded-lg text-center'>
-          <h2 className='text-2xl font-semibold mb-2'>New Arrivals</h2>
-          <p className='text-sm'>Check out the latest products in our collection.</p>
+        <div className='bg-gradient-to-r from-purple-400 via-pink-500 to-pink-600 p-6 rounded-lg text-center hover:scale-110 duration-300'>
+          <h2 className='text-2xl font-semibold mb-2 text-white'>New Arrivals</h2>
+          <p className='text-sm text-white'>Check out the latest products in our collection.</p>
         </div>
-        <div className='bg-green-200 p-6 rounded-lg text-center'>
-          <h2 className='text-2xl font-semibold mb-2'>Best Sellers</h2>
-          <p className='text-sm'>Discover the most popular items loved by our customers.</p>
+        <div className='bg-gradient-to-r from-purple-400 via-pink-500 to-pink-600 p-6 rounded-lg text-center hover:scale-110 duration-300'>
+          <h2 className='text-2xl font-semibold mb-2 text-white'>Best Sellers</h2>
+          <p className='text-sm text-white'>Discover the most popular items loved by our customers.</p>
         </div>
-        <div className='bg-yellow-200 p-6 rounded-lg text-center'>
-          <h2 className='text-2xl font-semibold mb-2'>Special Offers</h2>
-          <p className='text-sm'>Don't miss out on our exclusive deals and discounts.</p>
+        <div className='bg-gradient-to-r from-purple-400 via-pink-500 to-pink-600 p-6 rounded-lg text-center hover:scale-110 duration-300'>
+          <h2 className='text-2xl font-semibold mb-2 text-white'>Special Offers</h2>
+          <p className='text-sm text-white'>Don't miss out on our exclusive deals and discounts.</p>
         </div>
       </section>
-      
+
       {/* Featured Products Section */}
       <section className='featured-products'>
-        <h2 className='text-3xl font-bold text-center mb-3'>Featured Products</h2>
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6'>
-          {/* Add featured products here */}
+        <h2 className='text-3xl font-bold text-center text-purple-500'>Featured Products</h2>
+        <ImageSwitcher images={images} />
+        {/* Product Cards */}
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-10'>
+          <Card 
+            itemName="Iphone" 
+            details="Good Product" 
+            price={20} 
+            discount={50} 
+            image="i15.jpeg" 
+          />
+          <Card 
+            itemName="MacBook" 
+            details="Good Product" 
+            price={2000} 
+            discount={5} 
+            image="mac2.webp" 
+          />
+          <Card 
+            itemName="LG Smart" 
+            details="Good Product" 
+            price={2500} 
+            discount={15} 
+            image="tv1.jpeg" 
+          />
+          <Card 
+            itemName="Samsung S24 Ultra" 
+            details="Flagship Mobile" 
+            price={1400} 
+            discount={15} 
+            image="s24.jpeg" 
+          />
+          <Card 
+            itemName="I Watch" 
+            details="Series 9" 
+            price={500} 
+            discount={10} 
+            image="watch9.jpeg" 
+          />
+          <Card 
+            itemName="Car" 
+            details="Kid Product" 
+            price={100} 
+            discount={12} 
+            image="car.jpeg" 
+          />
         </div>
       </section>
-
-      {/* Image Switcher */}
-      <div className='my-4'>
-        <ImageSwitcher images={images} />
-      </div>
-
-      {/* Product Cards */}
-      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-10'>
-        <Card 
-          itemName="Iphone" 
-          details="Good Product" 
-          price={20} 
-          discount={50} 
-          image="i15.jpeg" 
-        />
-        <Card 
-          itemName="MacBook" 
-          details="Good Product" 
-          price={2000} 
-          discount={5} 
-          image="mac2.webp" 
-        />
-        <Card 
-          itemName="LG Smart" 
-          details="Good Product" 
-          price={2500} 
-          discount={15} 
-          image="tv1.jpeg" 
-        />
-        <Card 
-          itemName="Samsung S24 Ultra" 
-          details="Flagship Mobile" 
-          price={1400} 
-          discount={15} 
-          image="s24.jpeg" 
-        />
-        <Card 
-          itemName="I Watch" 
-          details="Series 9" 
-          price={500} 
-          discount={10} 
-          image="watch9.jpeg" 
-        />
-        <Card 
-          itemName="Car" 
-          details="Kid Product" 
-          price={100} 
-          discount={12} 
-          image="car.jpeg" 
-        />
-      </div>
-
-      
     </div>
   );
 }
