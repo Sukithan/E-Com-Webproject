b46ef33c595fd76d7219fbdd2b81944782b6b3d8
RahaviSiri <rahavi24siri@gmail.com>
2024-09-12 21:55:40 +0530

    Add changes

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -14,7 +14,7 @@ import Phone from "./Phone.jpg"
 import Television from "./Televison.jpg"
 import ToyCar from "./ToyCar.jpeg"
 import ContactUs from "./contactUs.jpg"
-import Electric_car from "./Electric car.jpg"
+import Electric_car from "./Electriic car.jpg"
 import Samsung_S24_Ultra from "./Samsung S24 Ultra.jpg"
 import I_watch from "./I-watch.jpg"
 
